9395c2b1a585040d5e7a528a953a24cd1763128f
linzhou <linzhou@xinc818.group>
2020-09-05 17:31:12 +0800

    git commit

--- src/main/java/com/datastructure/util/collection/map/CollectionUtils.java
+++ src/main/java/com/datastructure/util/collection/map/CollectionUtils.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.List;
 
 public class CollectionUtils {
+
     public static  boolean isNotEmpty(Collection collection) {
         return !isEmpty(collection);
     }
