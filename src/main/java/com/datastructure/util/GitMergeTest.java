95c561ba92db799a5307ab70913476459eb8719c
linzhou <linzhou@xinc818.group>
2020-09-14 14:47:55 +0800

    'gitTest'

---
+++ src/main/java/com/datastructure/util/GitMergeTest.java
@@ -0,0 +1,23 @@
+package com.datastructure.util;
+
+public class GitMergeTest {
+
+    private Integer gitAddress;
+    private String message;
+
+    public Integer getGitAddress() {
+        return gitAddress;
+    }
+
+    public void setGitAddress(Integer gitAddress) {
+        this.gitAddress = gitAddress;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
